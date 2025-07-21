99d2a859241e37288e7b04083a3e1605d2dde38d
DESKTOP-J1T8E70\davs <Davit.sahakyan@volo.global>
2025-07-22 01:30:10 +0400

    fix header styles and uncommented code

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,16 +1,16 @@
 import './header.css'
-// import Image from 'next/image'
+import Image from 'next/image'
 
 function Header() {
 
   return (
       <div className="header">
         <div className="avatar">
-            {/* <Image src="/assets/images/avatar.jpg" alt="Davit Sahakyan Avatar" width={100} height={100} /> */}
+            <Image src="/assets/images/avatar.jpg" alt="Davit Sahakyan Avatar" width={100} height={135} className='avatar-img'/>
         </div>
-        {/* <h5 className="dav-name mb-10">Davit Sahakyan</h5> */}
+        <h5 className="dav-name mb-10">Davit Sahakyan</h5>
         <div className="sm-text">
-            {/* Front-end Developer */}
+            Front-end Developer
         </div>
       </div>
   )
