f3dcba3cb0c22005e63ab5fb737945922c5a8543
DESKTOP-J1T8E70\davs <Davit.sahakyan@volo.global>
2025-07-22 01:28:06 +0400

    change column info view

--- src/app/components/columnProgressBar/columnInfo.tsx
+++ src/app/components/columnProgressBar/columnInfo.tsx
@@ -1,23 +1,23 @@
 import './columnInfo.css';
 import { ColumnInfoProps } from '../../interfaces/interfaces';
 
-function ColumnInfo(props: ColumnInfoProps) {
-  console.log(props, 'props');
+const ColumnInfo = (props: ColumnInfoProps) => {
   return (
     <div className='column-info-container'>
       <div className='column-info-heading'>
         <h2>{props.heading}</h2>
       </div>
+      <div className='column-info-wrapper'>
       <div className='column-info'>
         {props.items.map((item, index) => {
           return (
             <div className='timeline-item' key={index}>
+              <div className='rounded'></div>
+              <div className='triangle'></div>
               <div className='timeline-item-content'>
                 <div className="timeline-item-content-header">
-                  <h5>{item.title}</h5>
-                  <span className='tag' style={{ background: 'blue' }}>
-                    {item.period}
-                  </span>
+                  <span className='title'>{item.title}</span>
+                  <span className='period'>{item.period}</span>
                 </div>
                 <p>{item.description}</p>
               </div>
@@ -25,8 +25,12 @@ function ColumnInfo(props: ColumnInfoProps) {
           );
         })}
       </div>
+      <div className='separtor'>
+      </div>
+      </div>
     </div>
   );
 }
 
 export default ColumnInfo;
+
