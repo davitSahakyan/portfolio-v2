116e6155fa579a23823abf50274802a0eee7fff8
DESKTOP-J1T8E70\davs <Davit.sahakyan@volo.global>
2025-07-22 01:28:39 +0400

    added cv and styles for it

--- src/app/components/downloadCV/downloadCV.tsx
+++ src/app/components/downloadCV/downloadCV.tsx
@@ -1,14 +1,21 @@
-import DownloadIcon from '@mui/icons-material/Download';
-import './downloadCV.css';
+import DownloadIcon from "@mui/icons-material/Download";
+import "./downloadCV.css";
 
 const DownloadCv = () => {
   return (
     <div className="download pt-30">
-        <a href="/blank.pdf" download="blank.pdf" className="download-link">
-            <span>Download CV</span><DownloadIcon/>
-        </a>
+      <a
+        href="/DavitSahakyan.pdf"
+        download="Davit_Sahakyan_CV.pdf"
+        className="download-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span>Download CV</span>
+        <DownloadIcon />
+      </a>
     </div>
-  )
+  );
 };
 
 export default DownloadCv;
